720b9f0552a348bcc41b2111dcaf30dc58c724e2
Msierra001 <melvin.sierraic3@gmail.com>
2025-09-01 20:18:48 -0400

    Creating a window

--- main.cpp
+++ main.cpp
@@ -17,5 +17,16 @@ int main(){
     // initializing the GLFW library
     if( !glfwInit() ) return -1;
 
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
     return 0; 
 }
